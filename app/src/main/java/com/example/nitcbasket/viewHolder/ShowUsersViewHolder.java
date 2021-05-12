649ab0c8ecdac8ea96629410f349146daee6c71b
Apurva2412 <apurva.rathore2412@gmail.com>
2021-05-12 13:04:55 +0530

    Add all my files

---
+++ app/src/main/java/com/example/nitcbasket/viewHolder/ShowUsersViewHolder.java
@@ -0,0 +1,40 @@
+package com.example.nitcbasket.viewHolder;
+
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.nitcbasket.R;
+import com.example.nitcbasket.store.ItemClickListner;
+
+public class ShowUsersViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+
+    public TextView users_name, users_email, users_status;
+    public Button block_user;
+    public ImageView user_img;
+    public ItemClickListner itemListclik;
+
+    public ShowUsersViewHolder(@NonNull View itemView) {
+        super(itemView);
+
+        users_name = (TextView) itemView.findViewById(R.id.show_users_name);
+        users_email = (TextView) itemView.findViewById(R.id.show_users_email);
+        users_status = (TextView) itemView.findViewById(R.id.show_users_status);
+        block_user = (Button) itemView.findViewById(R.id.editblock_user);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        itemListclik.onClick(v,getAdapterPosition(),false);
+
+    }
+
+    public void setItemListclik(ItemClickListner itemListclik) {
+        this.itemListclik = itemListclik;
+    }
+}
